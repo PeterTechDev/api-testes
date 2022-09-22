3f6641a689cd70ab78a9a833f5a84eac241240c6
petertechdev <peterleite.dev@gmail.com>
2022-09-22 01:01:28 -0300

    Create User Repository

---
+++ src/main/java/com/petertech/api/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.petertech.api.repositories;
+
+import com.petertech.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
