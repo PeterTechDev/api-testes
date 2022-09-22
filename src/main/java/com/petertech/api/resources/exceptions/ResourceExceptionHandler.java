c24a2856a8852871b9bb714552f63749d75897cd
petertechdev <peterleite.dev@gmail.com>
2022-09-22 03:52:10 -0300

    Custom exception EmailAlreadyExist

--- src/main/java/com/petertech/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/petertech/api/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.petertech.api.resources.exceptions;
 
+import com.petertech.api.services.exceptions.DataIntegratyViolationException;
 import com.petertech.api.services.exceptions.ObjetctNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler {
                 new StandartError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), exception.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandartError> dataIntegrityViolation(DataIntegratyViolationException exception, HttpServletRequest request) {
+        StandartError error =
+                new StandartError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), exception.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/petertech/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/petertech/api/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.petertech.api.domain.User;
 import com.petertech.api.domain.dto.UserDTO;
 import com.petertech.api.repositories.UserRepository;
 import com.petertech.api.services.UserService;
+import com.petertech.api.services.exceptions.DataIntegratyViolationException;
 import com.petertech.api.services.exceptions.ObjetctNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return userRepository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj) {
+        Optional<User> user = userRepository.findByEmail(obj.getEmail());
+        if (user.isPresent() && !user.get().getId().equals(obj.getId())) {
+            throw new DataIntegratyViolationException("E-mail already exists");
+        }
     }
 }
