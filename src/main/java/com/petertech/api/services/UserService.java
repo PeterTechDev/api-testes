f1956a0a78d3f3a52dc5e45dad50688f96053768
petertechdev <peterleite.dev@gmail.com>
2022-09-22 03:44:48 -0300

    Create endpoint create user

--- src/main/java/com/petertech/api/services/UserService.java
+++ src/main/java/com/petertech/api/services/UserService.java
@@ -1,10 +1,12 @@
 package com.petertech.api.services;
 
 import com.petertech.api.domain.User;
+import com.petertech.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/com/petertech/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/petertech/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.petertech.api.services.impl;
 
 import com.petertech.api.domain.User;
+import com.petertech.api.domain.dto.UserDTO;
 import com.petertech.api.repositories.UserRepository;
 import com.petertech.api.services.UserService;
 import com.petertech.api.services.exceptions.ObjetctNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
        Optional<User> user = userRepository.findById(id);
@@ -25,5 +30,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return userRepository.save(mapper.map(obj, User.class));
     }
 }
