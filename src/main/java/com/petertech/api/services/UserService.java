280dd99f65775fcf781f348bb2b24030b4f366ef
petertechdev <peterleite.dev@gmail.com>
2022-09-25 19:20:26 -0300

    Endpoint delete by ID

--- src/main/java/com/petertech/api/services/UserService.java
+++ src/main/java/com/petertech/api/services/UserService.java
@@ -10,5 +10,5 @@ public interface UserService {
     List<User> findAll();
     User create(UserDTO obj);
     User update(UserDTO obj);
-//    void delete(Integer id);
+    void delete(Integer id);
 }
