243b76a301202fbaccf2f524c0ce9caadf671cf8
petertechdev <peterleite.dev@gmail.com>
2022-09-28 06:12:41 -0300

    Improve unitary test coverage for delete exception handler

--- src/test/java/com/petertech/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/com/petertech/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.petertech.api.resources.exceptions;
 
+import com.petertech.api.services.exceptions.DataIntegratyViolationException;
 import com.petertech.api.services.exceptions.ObjetctNotFoundException;
 import org.hibernate.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,12 +12,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String OBJECT_NOT_FOUND = "Object not found";
+    public static final String EMAIL_ALREADY_EXISTS = "E-mail already exists";
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -32,16 +36,37 @@ class ResourceExceptionHandlerTest {
                         new ObjetctNotFoundException(OBJECT_NOT_FOUND),
                         new MockHttpServletRequest());
 
+        response.getBody().getPath();
+
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals(404, response.getStatusCode().value());
+        assertEquals(404, response.getBody().getStatus());
         assertEquals(OBJECT_NOT_FOUND, response.getBody().getError());
         assertEquals(ResponseEntity.class, response.getClass());
         assertEquals(StandartError.class, response.getBody().getClass());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegrityViolation() {
+    void whenDataIntegrityViolationExceptionThenReturnResponseEntity() {
+        ResponseEntity<StandartError> response = exceptionHandler
+                .dataIntegrityViolation(
+                        new DataIntegratyViolationException(EMAIL_ALREADY_EXISTS),
+                        new MockHttpServletRequest());
+
+        response.getBody().getPath();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(400, response.getBody().getStatus());
+        assertEquals(EMAIL_ALREADY_EXISTS, response.getBody().getError());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandartError.class, response.getBody().getClass());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
+
 }
